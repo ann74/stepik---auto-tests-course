ea8c9797b537524484caad6694851862b635b569
ann74 <aulazar@yandex.ru>
2019-12-10 13:04:21 +0700

    Added files of practice tasks from selenium stepik block 3

---
+++ python_lesson3_2_step13.py
@@ -0,0 +1,38 @@
+﻿from selenium import webdriver
+import time
+import unittest
+import os.path
+
+options = webdriver.ChromeOptions()
+options.add_argument("--enable-features=NetworkServiceWindowsSandbox")
+options.add_argument("--disable-features=NetworkService")
+browser = webdriver.Chrome(chrome_options=options)
+
+def registration(link):
+    browser.get(link)
+    input1 = browser.find_element_by_css_selector(".first_block .first")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector(".first_block .second")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector(".first_block .third")
+    input3.send_keys("test@mail.ru")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    return welcome_text_elt.text
+
+class TestRegistr(unittest.TestCase):
+    def test_registr1(self):
+        text_registration = registration("http://suninjuly.github.io/registration1.html")
+        self.assertEqual(text_registration, "Поздравляем! Вы успешно зарегистировались!")
+
+    def test_registr2(self):
+        text_registration = registration("http://suninjuly.github.io/registration2.html")
+        self.assertEqual(text_registration, "Поздравляем! Вы успешно зарегистировались!")
+        
+if __name__ == "__main__":
+    unittest.main()
---
+++ python_lesson3_6_step2.py
@@ -0,0 +1,33 @@
+﻿import pytest
+from selenium import webdriver
+import time
+import math
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+links = [
+    ("https://stepik.org/lesson/236895/step/1"),
+    ("https://stepik.org/lesson/236896/step/1"),
+    ("https://stepik.org/lesson/236897/step/1"),
+    ("https://stepik.org/lesson/236898/step/1"),
+    ("https://stepik.org/lesson/236899/step/1"),
+    ("https://stepik.org/lesson/236903/step/1"),
+    ("https://stepik.org/lesson/236904/step/1"),
+    ("https://stepik.org/lesson/236905/step/1")
+]
+
+@pytest.mark.parametrize('link', links)
+def test_task(browser, link):
+    browser.get(link)
+    answer = str(math.log(int(time.time())))
+    browser.find_element_by_tag_name("textarea").send_keys(answer)
+    browser.find_element_by_css_selector("button.submit-submission").click()
+    fidbek = browser.find_element_by_css_selector(".smart-hints__hint").text
+    assert fidbek == "Correct!", fidbek
